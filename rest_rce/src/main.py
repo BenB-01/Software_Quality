b9e244f5fded970c98f27350cab713e4ec91b895
BenB-01 <benburkert3@gmail.com>
2025-01-14 16:51:08 +0100

    :construction: Add ASGI server to make FastAPI run in exe file

--- rest_rce/src/main.py
+++ rest_rce/src/main.py
@@ -1,3 +1,6 @@
+import multiprocessing
+
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +9,8 @@ app = FastAPI()
 @app.get('/')
 def read_root():
 	return {'Hello': 'World'}
+
+
+if __name__ == '__main__':
+	multiprocessing.freeze_support()  # For Windows support
+	uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=1)
