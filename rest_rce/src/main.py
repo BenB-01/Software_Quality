cabe62376748f5d86f36fc63906d9e4da0a8e061
Ben Carlos Burkert <benburkert3@gmail.com>
2025-01-15 11:20:59 +0100

    :package: Add FastAPI exe file that can be integrated into RCE

--- rest_rce/src/main.py
+++ rest_rce/src/main.py
@@ -13,4 +13,4 @@ def read_root():
 
 if __name__ == '__main__':
 	multiprocessing.freeze_support()  # For Windows support
-	uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=1)
+	uvicorn.run(app, host='127.0.0.1', port=8000, reload=False, workers=1)
